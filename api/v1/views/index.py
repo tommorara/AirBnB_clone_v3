5212754dfe61f66dfcd54caf7e18569ea864ff50
Tom Morara <morara1920@gmail.com>
2025-02-17 19:36:50 +0300

    Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,36 +1,41 @@
 #!/usr/bin/python3
-"""Define routes for blueprint
-"""
+
+"""API routes"""
 
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
-    """Return status of application
-    """
-    return jsonify({'status': 'OK'})
+    """Status"""
+    return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', strict_slashes=False)
 def stats():
-    """Retrieve count of objects in storage
-    """
-    from models.amenity import Amenity
-    from models.city import City
-    from models.place import Place
-    from models.review import Review
-    from models.state import State
-    from models.user import User
+    """Returns count of each object class in the database"""
 
-    classes = {"amenities": Amenity, "cities": City,
-               "places": Place, "reviews": Review,
-               "states": State, "users": User}
-    json_dict = {}
+    objects = {
+        "amenities": Amenity,
+        "cities": City,
+        "place": Place,
+        "reviews": Review,
+        "states": State,
+        "users": User
+    }
 
-    for name, cls in classes.items():
-        json_dict.update({name: storage.count(cls)})
+    objects_dict: dict = {}
 
-    return jsonify(json_dict)
+    for key, value in objects.items():
+        objects_dict[key] = storage.count(value)
+
+    return jsonify(objects_dict)
+
