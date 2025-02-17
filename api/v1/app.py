92eabd4e6f14ac530d1bc4c156379a9d9eb785f5
Mabongakev <kelvin.mabonga92@gmail.com>
2025-02-17 11:19:55 -0800

    task 3 updated

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,44 +1,35 @@
 #!/usr/bin/python3
 
-"""Shared API routes. Contains status and stats"""
+"""
+Creates the app. Lengthy documentation, blah, blah, blah.
+"""
 
 from api.v1.views import app_views
-from flask import jsonify
+from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
+from os import getenv
 
 
-@app_views.route('/status', strict_slashes=False)
-def status():
-    """Status API check. Returns status of API"""
-    return jsonify({"status": "OK"})
+app = Flask(__name__)
+app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
-@app_views.route('/stats', strict_slashes=False)
-def stats():
-    """Returns count of each object class in the database"""
+@app.teardown_appcontext
+def teardown(self):
+    """Tears down the storage"""
+    storage.close()
 
-    objects = {
-        "amenities": Amenity,
-        "cities": City,
-        "places": Place,
-        "reviews": Review,
-        "states": State,
-        "users": User
-    }
 
-    objects_dict = {}
-
-    for key, value in objects.items():
-        objects_dict[key] = storage.count(value)
-
-    return jsonify(objects_dict)
+@app.errorhandler(404)
+def page_not_found(error):
+    """return 404"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
-    pass
+    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = int(getenv('HBNB_API_PORT', '5000'))
+
+    app.run(host=HOST, port=PORT, threaded=True)
