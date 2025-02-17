74707b7d1b7215d272970af8530795548f2d5334
Tom Morara <morara1920@gmail.com>
2025-02-17 18:28:16 +0300

    Improve storage

--- models/state.py
+++ models/state.py
@@ -1,34 +1,41 @@
 #!/usr/bin/python3
-""" holds class State"""
-import models
-from models.base_model import BaseModel, Base
-from models.city import City
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
+""" State Module for HBNB project """
+
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
+import os
+import models
 
 
 class State(BaseModel, Base):
-    """Representation of state """
-    if models.storage_t == "db":
+    """ State class """
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
+        cities = relationship(
+            "City", cascade="all, delete", back_populates="state")
     else:
         name = ""
 
-    def __init__(self, *args, **kwargs):
-        """initializes state"""
-        super().__init__(*args, **kwargs)
-
-    if models.storage_t != "db":
         @property
         def cities(self):
-            """getter for list of city instances related to the state"""
-            city_list = []
-            all_cities = models.storage.all(City)
-            for city in all_cities.values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+            """FileStorage cities getter attribute"""
+            all_objects = models.storage.all()
+
+            all_cities = []
+
+            for k, v in all_objects.items():
+                try:
+                    if all_objects[k].state_id == self.id:
+                        all_cities.append(all_objects[k])
+                except AttributeError:
+                    pass
+
+            return all_cities
+
+    def __init__(self, *args, **kwargs):
+        """Initialize State"""
+        super().__init__(*args, **kwargs)
