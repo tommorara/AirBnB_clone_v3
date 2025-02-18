1e21f20b60d2fca447b3e6b3fb1c3637a62a6b20
Tom Morara <morara1920@gmail.com>
2025-02-18 21:38:49 +0300

    done

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,33 +1,45 @@
 #!/usr/bin/python3
-"""Flask web application
+"""
+app
 """
 
 from flask import Flask, jsonify
-from models import storage
-from api.v1.views import app_views
-from os import getenv
 from flask_cors import CORS
+from os import getenv
+
+from api.v1.views import app_views
+from models import storage
+
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown(error):
-    """Clean-up method
+def teardown(exception):
+    """
+    teardown function
     """
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
-    """Custom 404 error
+def handle_404(exception):
     """
-    return jsonify({'error': 'Not found'}), 404
+    handles 404 error
+    :return: returns 404 json
+    """
+    data = {
+        "error": "Not found"
+    }
 
-if __name__ == '__main__':
-    app.run(host=getenv('HBNB_API_HOST'),
-            port=getenv('HBNB_API_PORT'),
-            threaded=True)
+    resp = jsonify(data)
+    resp.status_code = 404
+
+    return(resp)
+
+if __name__ == "__main__":
+    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
